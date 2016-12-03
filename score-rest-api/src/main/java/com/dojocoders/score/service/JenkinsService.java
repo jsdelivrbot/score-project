83c4850af5440d9f57ae91153f1d1d2d608bba02
Emmanuel Vasseur <vasseur.emmanuel@gmail.com>
2016-12-03 15:50:46 +0100

    Fix jenkins job launch, and add logging on launch response

--- score-rest-api/src/main/java/com/dojocoders/score/service/JenkinsService.java
+++ score-rest-api/src/main/java/com/dojocoders/score/service/JenkinsService.java
@@ -1,9 +1,15 @@
 package com.dojocoders.score.service;
 
 import com.google.common.base.Throwables;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.util.EntityUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
@@ -11,15 +17,28 @@ import java.io.IOException;
 @Service
 public class JenkinsService {
 
+	public static final Logger LOG = LoggerFactory.getLogger(JenkinsService.class);
+
+    private static final HttpClient CLIENT = HttpClientBuilder.create().build();
+
     public void launchJobJenkins(String jenkinsUrl, String jenkinsJobName, String jenkinsJobToken) {
-        HttpPost post = new HttpPost(jenkinsUrl + "/job/" + jenkinsJobName + "/build?token=" + jenkinsJobToken);
-        HttpClient client = HttpClientBuilder.create().build();
+        HttpPost post = new HttpPost(jenkinsUrl + "/buildByToken/build?job=" + jenkinsJobName + "&token=" + jenkinsJobToken);
         try {
-            client.execute(post);
+            HttpResponse postResponse = CLIENT.execute(post);
+            LOG.info("status of call {} : {}", post, postResponse.getStatusLine().getStatusCode());
+
+            if(hasFailed(postResponse)) {
+                LOG.error("raison of fail : {}", postResponse.getStatusLine().getReasonPhrase());
+                LOG.warn("body of fail : {}", EntityUtils.toString(postResponse.getEntity()));
+            }
         } catch (IOException e) {
+            LOG.error("exception when call " + post + " :", e);
             Throwables.propagate(e);
         }
+    }
 
-        return;
+	/** @see {@link HttpStatus} */
+    private boolean hasFailed(HttpResponse requestResponse) {
+    	return requestResponse.getStatusLine().getStatusCode() < 200 || requestResponse.getStatusLine().getStatusCode() >= 300;
     }
 }
