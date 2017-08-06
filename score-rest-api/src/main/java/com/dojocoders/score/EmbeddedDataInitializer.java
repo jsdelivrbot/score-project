5c7c2df125c80580d8847564c1e05e43a8d9bc89
Gilles Vivies <gilles.vivies@gmail.com>
2017-08-06 12:03:00 +0200

    Split data initializers

--- score-rest-api/src/main/java/com/dojocoders/score/EmbeddedDataInitializer.java
+++ score-rest-api/src/main/java/com/dojocoders/score/EmbeddedDataInitializer.java
@@ -1,11 +1,5 @@
 package com.dojocoders.score;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Date;
-import java.util.List;
-import java.util.stream.Collectors;
-
 import javax.annotation.PostConstruct;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,18 +8,10 @@ import org.springframework.stereotype.Component;
 
 import com.dojocoders.score.api.ScoreController;
 import com.dojocoders.score.api.TeamController;
-import com.dojocoders.score.api.TripController;
-import com.dojocoders.score.model.trip.Grid;
-import com.dojocoders.score.model.trip.Item;
-import com.dojocoders.score.model.trip.Location;
-import com.dojocoders.score.model.trip.Trip;
-import com.google.common.collect.Lists;
 
 @Component
 @Profile("embedded-persistence")
 public class EmbeddedDataInitializer {
-
-	private static String ITEMS = "AaMmEeBb"; // Asteorid/Meteorite, Enemy, Bomb
 	
 	@Autowired
 	private TeamController teamController;
@@ -33,13 +19,10 @@ public class EmbeddedDataInitializer {
 	@Autowired
 	private ScoreController scoreController;
 	
-	@Autowired
-	private TripController tripController;
-	
 	@PostConstruct
 	public void init() {
-		setupTeam("Green Bay Packers");
 		setupTeam("New England Patriots");
+		setupTeam("Green Bay Packers");
 		setupTeam("Pittsburgh Steelers");
 		setupTeam("Dallas Cowboys");
 		setupTeam("Philadelphia Snakes");
@@ -48,50 +31,6 @@ public class EmbeddedDataInitializer {
 	private void setupTeam(String name) {
 		teamController.addTeam(name);
 		scoreController.addScoreTeamPoints(name, name.length());
-		setupTrip(name);
-	}
-	
-	private void setupTrip(String team) {
-		Trip trip = new Trip();
-		trip.setMessages(buildMessages(team));
-		trip.setGrid(buildGrid(team));
-		trip.setTeam(team);
-		Date date = new Date();
-		trip.setId(String.valueOf(date.getTime()));
-		tripController.addTrip(trip);
-	}
-	
-	private List<String> buildMessages(String team) {
-		List<String> msg = new ArrayList<String>();
-		msg.add(team + "! Gonna crush you !");
-		return msg;
-	}
-	
-	private Grid buildGrid(String team) {
-		Grid grid = new Grid();
-		grid.setHeight(team.length() * 2);
-		grid.setWidth(team.length() * 2);
-		grid.setContent(buildGridContent(team));
-		return grid;
-	}
-	
-	private List<Item> buildGridContent(String team) {
-		return Arrays.stream(team.split("")) //
-			.filter(letter -> letterIsAnItem(letter))
-			.map(letter -> buildItem(letter, team))
-			.collect(Collectors.toList());
-	}
-	
-	private boolean letterIsAnItem(String letter) {
-		return (ITEMS.indexOf(letter) > -1);
-	}
-	
-	private Item buildItem(String letter, String team) {
-		Double x = new Double(Math.random() * team.length());
-		Double y = new Double(Math.random() * team.length());
-		Location position = new Location(x.intValue(), y.intValue());
-
-		return new Item(letter.toUpperCase(), position);
 	}
 	
 }
---
+++ score-rest-api/src/main/java/com/dojocoders/score/EmbeddedTripInitializer.java
@@ -0,0 +1,90 @@
+package com.dojocoders.score;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+
+import com.dojocoders.score.api.TeamController;
+import com.dojocoders.score.api.TripController;
+import com.dojocoders.score.model.trip.Grid;
+import com.dojocoders.score.model.trip.Item;
+import com.dojocoders.score.model.trip.Location;
+import com.dojocoders.score.model.trip.Trip;
+
+@Component
+@Profile("embedded-persistence")
+public class EmbeddedTripInitializer {
+
+	private static String ITEMS = "AaMmEeBb"; // Asteorid/Meteorite, Enemy, Bomb
+	
+	@Autowired
+	private TeamController teamController;
+	
+	@Autowired
+	private TripController tripController;
+	
+	@SuppressWarnings("unused") // Injection ensures that teams are initialized before trips
+	@Autowired
+	private EmbeddedDataInitializer dataInitializer; 
+	
+	@PostConstruct
+	public void init() {
+		setupTrips();
+	}
+	
+	private void setupTrips() {
+		teamController.teams().forEach(team -> setupTrip(team));
+	}
+	
+	private void setupTrip(String team) {
+		Trip trip = new Trip();
+		trip.setMessages(buildMessages(team));
+		trip.setGrid(buildGrid(team));
+		trip.setTeam(team);
+		Date date = new Date();
+		trip.setId(String.valueOf(date.getTime()));
+		tripController.addTrip(trip);
+	}
+	
+	private List<String> buildMessages(String team) {
+		List<String> msg = new ArrayList<String>();
+		msg.add(team + "! Gonna crush you !");
+		return msg;
+	}
+	
+	private Grid buildGrid(String team) {
+		Grid grid = new Grid();
+		grid.setHeight(team.length() * 2);
+		grid.setWidth(team.length() * 2);
+		grid.setContent(buildGridContent(team));
+		return grid;
+	}
+	
+	private List<Item> buildGridContent(String team) {
+		return Arrays.stream(team.split("")) //
+			.filter(letter -> letterIsAnItem(letter))
+			.map(letter -> buildItem(letter, team))
+			.collect(Collectors.toList());
+	}
+	
+	private boolean letterIsAnItem(String letter) {
+		return (ITEMS.indexOf(letter) > -1);
+	}
+	
+	private Item buildItem(String letter, String team) {
+		Double x = new Double(Math.random() * team.length());
+		Double y = new Double(Math.random() * team.length());
+		Location position = new Location(x.intValue(), y.intValue());
+
+		return new Item(letter.toUpperCase(), position);
+	}
+}
+
