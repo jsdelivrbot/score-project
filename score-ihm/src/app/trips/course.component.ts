21973ace773c461730a833fb7bf82089effd87a2
Gilles Vivies <gilles.vivies@gmail.com>
2017-08-10 23:04:54 +0200

    Change static objects colors for icons

--- score-ihm/src/app/trips/course.component.ts
+++ score-ihm/src/app/trips/course.component.ts
@@ -2,7 +2,7 @@ import { Observable } from 'rxjs/Observable';
 import { TripAnimation } from './animations';
 import { GridItem, Grid, Location } from '../trips/trip.model';
 import { Component, OnInit, Input, ElementRef, ViewChild, Renderer2, AfterViewInit } from '@angular/core';
-import {TweenLite} from 'gsap';
+import { TweenLite } from 'gsap';
 
 @Component({
   selector: 'app-course',
@@ -19,17 +19,18 @@ export class CourseComponent implements AfterViewInit, OnInit {
 
   private gridContainer: any;
   private shipContainer: any;
-  private viewBox = '0 0 100 100';
-  graphicalItems: any[];
-  graphicalLocations: any[];
+  staticObjects: any[];
+  shipPositions: any[];
   stars: any[];
   randomArray: any[];
   cellWidth = 20;
   cellHeight = 20;
   gridWidth = 0;
   gridHeight = 0;
-  squareSide = 0;
+  squareSize = 0;
+  objectSize = 0;
   svgPath: string;
+  staticObjectsSvgPath: string;
   shipPath: string;
   shipPos: string;
   shipSize: string;
@@ -42,8 +43,8 @@ export class CourseComponent implements AfterViewInit, OnInit {
   }
 
   ngOnInit() {
-    this.graphicalItems = [];
-    this.graphicalLocations = [];
+    this.staticObjects = [];
+    this.shipPositions = [];
     this.courseVisible = 'false';
     this.stars = [];
   }
@@ -68,88 +69,87 @@ export class CourseComponent implements AfterViewInit, OnInit {
   private calculateGridSize(): void {
     this.gridWidth = (this.gridContainer.clientHeight - 2) / this.grid.getWidth();
     this.gridHeight = (this.gridContainer.clientHeight - 2) / this.grid.getHeight();
-    this.squareSide = Math.round((this.gridWidth > this.gridHeight) ? this.gridHeight : this.gridWidth);
-    this.svgPath = 'M ' + this.squareSide + ' 0 L 0 0 0 ' + this.squareSide;
+    this.squareSize = Math.round((this.gridWidth > this.gridHeight) ? this.gridHeight : this.gridWidth);
+    this.objectSize = this.squareSize * 1.5;
+    this.svgPath = 'M ' + this.squareSize + ' 0 L 0 0 0 ' + this.squareSize;
+    this.staticObjectsSvgPath = 'M 0 0 H ' + this.objectSize + ' V 0 H '
+      + this.objectSize + ' V ' + this.objectSize + ' H 0 V ' + this.objectSize + ' H 0 V 0';
   }
 
   private placeObjectsOnGrid(): void {
-    this.graphicalItems = this.grid.getContent().map(it => this.addObjectGraphicalAttributes(it, this.squareSide));
+    this.staticObjects = this.grid.getContent()
+      .map(it => this.addObjectGraphicalAttributes(it, this.squareSize));
   }
 
   private drawCourseOnGrid(): void {
-    this.graphicalLocations = this.course.map(it => this.addCourseGraphicalAttributes(it, this.squareSide));
+    this.shipPositions = this.course
+      .map(it => this.addCourseGraphicalAttributes(it, this.squareSize));
   }
 
   private addObjectGraphicalAttributes(item: GridItem, objectSize: number): any {
     return {
       object: item.getObject(),
-      posX: (item.getLocation().getPosX() * this.squareSide) + (this.squareSide / 2),
-      posY: (item.getLocation().getPosY() * this.squareSide) + (this.squareSide / 2),
-      size: this.squareSide,
-      ray: this.squareSide / 2,
-      color: this.getColorClass(item.getObject())
+      posX: (item.getLocation().getPosX() * this.squareSize) + (this.objectSize / 2),
+      posY: (item.getLocation().getPosY() * this.squareSize) + (this.objectSize / 2),
+      size: this.objectSize,
+      url: this.getObjectUrl(item.getObject())
     };
   }
 
   private addCourseGraphicalAttributes(location: Location, objectSize: number): any {
     return {
-      posX: (location.getPosX() * this.squareSide) + (this.squareSide / 2),
-      posY: (location.getPosY() * this.squareSide) + (this.squareSide / 2),
-      size: this.squareSide,
-      ray: this.squareSide / 4,
-      color: this.getColorClass('L')
+      posX: (location.getPosX() * this.squareSize) + (this.squareSize / 2),
+      posY: (location.getPosY() * this.squareSize) + (this.squareSize / 2),
+      size: this.squareSize,
+      ray: this.squareSize / 4,
+      color: 'color-location'
     };
   }
 
   private placeSomeStars(nb: number) {
     Observable.from(Array(nb)) //
-              .forEach(elt =>
-                this.stars.push({ posX: this.getRandomGridPos(), posY: this.getRandomGridPos(), size: 1, ray: 1, color: 'white' } ));
+      .forEach(elt =>
+        this.stars.push({ posX: this.getRandomGridPos(), posY: this.getRandomGridPos(), size: 1, ray: 1, color: 'white' }));
   }
 
   private getRandomGridPos(): number {
-    return Math.floor(Math.random() * this.grid.getWidth() * this.squareSide) + 1;
+    return Math.floor(Math.random() * this.grid.getWidth() * this.squareSize) + 1;
   }
 
   private startShipTrip() {
-
     const subs = Observable
       .interval(100)
       .map(pos => pos + 1).subscribe(
-        pos => {
-          const posX = (this.course[pos].getPosX() * this.squareSide) + (this.squareSide / 2);
-          const posY = (this.course[pos].getPosY() * this.squareSide) + (this.squareSide / 2);
-          TweenLite.to(this.shipContainer, .2, {x: posX , y: posY });
-          if (pos === (this.course.length - 1)) {
-            subs.unsubscribe();
-          }
-        });
+      pos => {
+        const posX = (this.course[pos].getPosX() * this.squareSize) + (this.squareSize / 2);
+        const posY = (this.course[pos].getPosY() * this.squareSize) + (this.squareSize / 2);
+        TweenLite.to(this.shipContainer, .2, { x: posX, y: posY });
+        if (pos === (this.course.length - 1)) {
+          subs.unsubscribe();
+        }
+      });
   }
 
-  private getColorClass(obj: string): string {
-    let className = 'color-';
+  private getObjectUrl(obj: string): string {
+    let objectUrl = 'url(';
     switch (obj) {
       case 'B': {
-        className = className + 'bomb';
+        objectUrl = objectUrl + '#mineimg';
         break;
       }
       case 'E': {
-        className = className + 'enemy';
+        objectUrl = objectUrl + '#enemyimg';
         break;
       }
       case 'A': {
-        className = className + 'asteroid';
+        objectUrl = objectUrl + '#meteoriteimg';
         break;
       }
       case 'M': {
-        className = className + 'meteorite';
-        break;
-      }
-      case 'L': {
-        className = className + 'location';
+        objectUrl = objectUrl + '#meteoriteimg';
         break;
       }
     }
-    return className;
+    return objectUrl + ')';
   }
 }
